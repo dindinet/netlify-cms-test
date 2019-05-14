6c683785bb36cf9b0a55664d4baa93ef304c077a
dindinet <github.dklongley@neverbox.com>
2019-05-14 12:06:23 +0100

    try a template

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -7,6 +7,8 @@ export default class HomePreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
-   return <img src="{ image }" />
+   return <div>
+            <img src='{ getAsset(entry.getIn(["data", "image"]))}' />
+            </div>
    }
 }

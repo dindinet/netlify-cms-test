4fcba09c6e46fced30e1d801a61a2374ff48eb37
dindinet <github.dklongley@neverbox.com>
2019-05-14 12:50:16 +0100

    jumbotron

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -4,7 +4,7 @@ export default class Jumbotron extends React.Component {
   render() {
     const {image, title, subtitle, text} = this.props;
     return <div>
-<section id="fp-hero" class="hero is-medium" style={{backgroundImage: image && `url(${image});background-size: cover; background-position: center center;`}}">
+<section id="fp-hero" class="hero is-medium" style={{backgroundImage: image && `url(${image});background-size: cover; background-position: center center;`}}>
   <div class="hero-body">
      <div class="container has-text-centered">
    <h2 class="is-size-1">{ title }</h2>
